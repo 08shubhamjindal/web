eab9f3f736db52386c16f9fd2dd5306558cc0d75
08shubhamjindal <shubhamjindal.cse19@jecrc.ac.in>
2017-03-28 00:14:00 +0530

    add form

---
+++ file.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<title>
+	files</title>
+	<head></head>
+	<body>
+		<?php
+		if (isset($_POST['submitted'])) {
+			if (!empty($_POST['quote']) && ($_POST['quote'] !='enter quote here') ) {
+				
+				if($fp=fopen('../quotes.txt', 'ab' ))
+				{
+					stream_encoding($fb , 'utf-8');
+					fwrite($fb,"{$_POST['quote']}");
+					fclose($fb);
+					print "<p> your quote has been stored</p>"; 
+				}
+				else
+				{
+					print'<p style="color:red;"> your quote could not be stored due to to a system error.</p>';
+				}
+			}
+              else{
+              	print '<p style="color:red;">please enter a quote';
+              }	
+		}
+		?>
+		<form action = "file.php" method="post">
+			<textarea name="quote" rows="5" cols="30">enter a quotation here</textarea>
+	<input type ="submit" name="submit" value="calc"/>
+		<input type ="hidden" name="submitted" value ="true" />
+	</form>
+	</body>
+</html>
