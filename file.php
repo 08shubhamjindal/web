e6cf21a33391b8c59ebd7d184a4989179e68e1ad
08shubhamjindal <shubhamjindal.cse19@jecrc.ac.in>
2017-03-28 18:11:44 +0530

    lock or unlock

--- file.php
+++ file.php
@@ -5,13 +5,16 @@
 	<head></head>
 	<body>
 		<?php
-		if (isset($_POST['submitted'])) {
-			if (!empty($_POST['quote']) && ($_POST['quote'] !='enter quote here') ) {
-				
+		if (isset($_POST['submitted'])) 
+		{
+			if (!empty($_POST['quote']) && ($_POST['quote'] !='enter quote here') ) 
+			{
 				if($fp=fopen('../quotes.txt', 'ab' ))
 				{
 					stream_encoding($fb , 'utf-8');
+		 			flock($fp, LOCK_EX);
 					fwrite($fb,"{$_POST['quote']}");
+	                flock($fp, LOCK_UN);
 					fclose($fb);
 					print "<p> your quote has been stored</p>"; 
 				}
@@ -20,12 +23,13 @@
 					print'<p style="color:red;"> your quote could not be stored due to to a system error.</p>';
 				}
 			}
-              else{
+              else
+              {
               	print '<p style="color:red;">please enter a quote';
               }	
 		}
 		?>
-		<form action = "file.php" method="post">
+		    <form action = "file.php" method="post">
 			<textarea name="quote" rows="5" cols="30">enter a quotation here</textarea>
 	<input type ="submit" name="submit" value="calc"/>
 		<input type ="hidden" name="submitted" value ="true" />
